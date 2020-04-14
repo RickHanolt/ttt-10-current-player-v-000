381c95deffeb75c384c073ac9df09f1ea5634ed8
Richard Hanolt <rhanolt@gmail.com>
2020-04-14 15:16:13 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -5,5 +5,12 @@ def turn_count(board)
       counter += 1
     end
   end
-  return counter
+  counter
+end
+
+def current_player(board)
+  if turn_count(board)%2 == 0
+    "X"
+  else
+    "O"
 end
